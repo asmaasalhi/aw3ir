48ae51145a12851491db87537bea9f5fd0f78b08
asmaasalhi <72708160+asmaasalhi@users.noreply.github.com>
2020-11-02 15:09:52 +0100

    Update formulaire.js
    enlever smile

--- tp2/formulaire.js
+++ tp2/formulaire.js
@@ -41,7 +41,7 @@ function validation(){
       
      else
      
-        document.getElementById("resultat").innerHTML = "Bienvenue " + document.querySelector("#prenom").value + " 😀";
+        document.getElementById("resultat").innerHTML = "Bienvenue " + document.querySelector("#prenom").value;
      
      
      }
